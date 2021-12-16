cf8b1e422e796ba317c5ecd5fc5f7a2b18e4ee01
atharvavijaysawant <96255853+atharvavijaysawant@users.noreply.github.com>
2021-12-16 22:32:57 +0530

    Add files via upload

---
+++ date and time.py
@@ -0,0 +1,7 @@
+import datetime
+date=datetime.datetime.now()
+print(date)
+print(date.strftime("%d-%m-%y"))
+print(date.strftime("%A"))
+print(date.strftime("%b"))
+print(date.strftime("%y"))
---
+++ factorial.py
@@ -0,0 +1,4 @@
+fact=1
+for i in range(1,5):
+    fact*=i
+print(fact)
